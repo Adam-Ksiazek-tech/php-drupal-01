e95efc53b8ba35a6e34a72e08fd5e81e040aec36
adam-ksiazek <a.ksiazek@pap.pl>
2025-07-15 11:29:09 +0200

    First controller - https://www.youtube.com/watch?v=4_r_1W6aVIE

--- drupal/web/modules/custom/mymodule/src/Controller/FirstController.php
+++ drupal/web/modules/custom/mymodule/src/Controller/FirstController.php
@@ -1,21 +1,21 @@
 <?php
 
-/**
- * @file
- * Generates markup to be display. Functionality in this Controlleris
- * wired to Drupal in mymodule.routing.yml
- */
-
 namespace Drupal\mymodule\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class FirstController extends ControllerBase {
   public function simpleContent() {
     return [
       '#type' => 'markup',
-      '#markup' => t('Hello Drupal world.
-                            Time flies like an arrow, fruit flies like a banana.'),
+      '#markup' => $this->t('Hello Drupal world. Time flies like an arrow, fruit flies like a banana.'),
     ];
   }
+
+  public function sayHelloJSON() {
+    $data = ['hello' => 'world'];
+    return new JsonResponse($data);
+  }
+
 }
