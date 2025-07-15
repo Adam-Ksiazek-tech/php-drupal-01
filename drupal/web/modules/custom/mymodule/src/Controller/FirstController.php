4e038a5de0ed99138ddcbe1cd02c7caa56cda29e
adam-ksiazek <a.ksiazek@pap.pl>
2025-07-15 12:29:46 +0200

    First controller - https://www.youtube.com/watch?v=4_r_1W6aVIE

--- drupal/web/modules/custom/mymodule/src/Controller/FirstController.php
+++ drupal/web/modules/custom/mymodule/src/Controller/FirstController.php
@@ -4,6 +4,7 @@ namespace Drupal\mymodule\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class FirstController extends ControllerBase {
   public function simpleContent() {
@@ -11,6 +12,10 @@ class FirstController extends ControllerBase {
       '#type' => 'markup',
       '#markup' => $this->t('Hello Drupal world. Time flies like an arrow, fruit flies like a banana.'),
     ];
+  }
+
+  public function sayHello() {
+    return new Response('<html><body>Hello Drupal world.</body></html>');
   }
 
   public function sayHelloJSON() {
