92cbf2804b5ecbe91f5fa06b40f071511e844cc8
adam-ksiazek <a.ksiazek@pap.pl>
2025-07-17 17:51:49 +0200

    RSVPBlock

---
+++ drupal/web/modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Creatres a block which displays the RTVPForm contained in RSVPForm.php
+ */
+
+namespace Drupal\rsvplist\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * Provides the RSVP mian block.
+ *
+ * @Block(
+ *   id = "rsvp_block",
+ *   admin_label = @Translation("The RSVP Block")
+ * )
+ */
+class RSVPBlock extends BlockBase {
+
+}
