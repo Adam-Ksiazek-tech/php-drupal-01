941182ba355ac1516ca53fe7cb70b46fa4ba4198
adam-ksiazek <a.ksiazek@pap.pl>
2025-07-18 17:17:11 +0200

    ReportController.php - load - $database = \Drupal::database();

---
+++ drupal/web/modules/custom/rsvplist/src/Controller/ReportController.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Privide site administrators with a list of all the RSVP List signups
+ * so they know who is attending their events.
+ */
+
+namespace Drupal\rsvplist\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Database;
+
+class ReportController extends ControllerBase {
+
+  /**
+   * Gets and returns all RSVPs for all nodes.
+   * These are returned as an associative array, with each row
+   * containing the username, the node title, and email of RSVP.
+   *
+   * @return array|null
+   */
+  protected function load() {
+    try {
+      $database = \Drupal::database();
+      $select_query = $database->select('rsvplist', 'r');
+
+      $select_query->join('users_field_data', 'u', 'r.uid = u.uid');
+      $select_query->join('node_field_data', 'n', 'r.nid = n.nid');
+
+      $select_query->addField('u', 'name', 'username');
+      $select_query->addField('n', 'title');
+      $select_query->addField('r', 'mail');
+
+    }
+    catch (\Exception $e) {
+
+    }
+  }
+}
+
+
