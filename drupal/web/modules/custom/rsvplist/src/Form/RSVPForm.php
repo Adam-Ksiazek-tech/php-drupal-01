895e8cfd07ba33abec68373477c34ce954b4c784
adam-ksiazek <a.ksiazek@pap.pl>
2025-07-21 16:06:49 +0200

    RSVPForm.php - $node: check if exists

--- drupal/web/modules/custom/rsvplist/src/Form/RSVPForm.php
+++ drupal/web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -24,9 +24,9 @@ class RSVPForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $node = \Drupal::routeMatch()->getParameter('node');
-//    \Drupal::messenger()->addMessage(t("form_id ---@id---", ['@id' => $form_id] ));
-    \Drupal::messenger()->addMessage($this->t('RSVPForm->buildForm, a tu node:@node', ['@node' => $node->label()]));
+
     if ( !(is_null($node)) ) {
+      \Drupal::messenger()->addMessage($this->t('RSVPForm->buildForm, a tu node:@node', ['@node' => $node->label()]));
       $nid = $node->id();
     }
     else {
