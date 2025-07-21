b0221d004ca82613ef8b3f6499095e307f1cab41
adam-ksiazek <a.ksiazek@pap.pl>
2025-07-21 10:38:14 +0200

    rsplist - EnablerService.php

--- drupal/web/modules/custom/rsvplist/src/EnablerService.php
+++ drupal/web/modules/custom/rsvplist/src/EnablerService.php
@@ -16,4 +16,32 @@ class EnablerService {
   public function __construct(Connection $connection) {
     $this->database_connection = $connection;
   }
+
+  /**
+   * Checks if an individual node is RSVP functionality
+   *
+   * @param Node $node
+   * @return bool
+   * whether or not node is enabled for the RSVP functionality.
+   */
+  public function isEnabled(Node &$node) {
+    if ($node->isNew()) {
+      return FALSE;
+    }
+    try {
+      $select = $this->database_connection->select("rsvplist_enabled', 're');
+      $select->fields('re', ['nid']);
+      $select->condition('nid', $node->id());
+      $results = $select->execute();
+
+      return !(empty( $results->fetchCol() ));
+
+    }
+    catch ( \Exception $e ) {
+      \Drupal::messenger()->addError(
+      $this->t('Unable to determine RSVP settings at this time.'));
+
+      return NULL;
+    }
+  }
 }
